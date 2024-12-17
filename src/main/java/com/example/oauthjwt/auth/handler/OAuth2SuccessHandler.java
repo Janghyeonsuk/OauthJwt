9dd7bcdbf14a594f7fea09be0d1fe2d12c0690e8
Hyeonsuk <vkdnjdjxor@naver.com>
2024-12-17 12:32:04 +0900

    Update .gitignore

--- src/main/java/com/example/oauthjwt/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/example/oauthjwt/auth/handler/OAuth2SuccessHandler.java
@@ -27,19 +27,32 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException, ServletException {
         log.info("Oauth 성공 핸들러");
 
         String accessToken = tokenProvider.generateAccessToken(authentication);
         tokenProvider.generateRefreshToken(authentication, accessToken);
 
-        response.setHeader(AUTHORIZATION, TokenKey.TOKEN_PREFIX + accessToken);
-        response.setHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        // Authorization 헤더에 Bearer 토큰 설정
+        response.setHeader(AUTHORIZATION, TokenKey.TOKEN_PREFIX + accessToken); // "Bearer " 접두어 추가
+        response.setContentType("application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-        String redirectUrl = UriComponentsBuilder.fromUriString(URI)
-                .queryParam("accessToken", accessToken)
-                .build().toUriString();
+        // 응답 바디에 성공 메시지와 필요한 정보 반환
+        response.getWriter().write("{"
+                + "\"message\": \"Authentication successful\", "
+                + "\"tokenType\": \"Bearer\", "
+                + "\"accessToken\": \"" + accessToken + "\""
+                + "}");
+        response.getWriter().flush();
 
-        response.sendRedirect(redirectUrl);
+//        response.setHeader(AUTHORIZATION, TokenKey.TOKEN_PREFIX + accessToken);
+//        response.setHeader("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+//
+//        String redirectUrl = UriComponentsBuilder.fromUriString(URI)
+//                .queryParam("accessToken", accessToken)
+//                .build().toUriString();
+//
+//        response.sendRedirect(redirectUrl);
     }
 }
